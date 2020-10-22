13386f960899e95dbf3e396c70b31ac9c55c649a
Freddy Rondon <frondonfermin@gmail.com>
2020-10-22 09:40:24 -0400

    Creating books and sorting by title

--- src/ClientPTUI.java
+++ src/ClientPTUI.java
@@ -1,3 +1,8 @@
+import Books.Book;
+import Books.BookSorter;
+import Books.OrderStrategies.OrderByTitle;
+
+import java.util.*;
 
 /**
  * This class represents the client and it performs
@@ -5,5 +10,26 @@
  * @author Freddy Rondon
  */
 public class ClientPTUI {
+
+    public static void main(String[] args) {
+        List<Book> books = new ArrayList<>();
+        for (int i=0; i < 10; i++){
+            books.add(new Book("Book #"+i,
+                    "Author #"+i,
+                    "ISBN #"+i,
+                    100+i,
+                    5+i,
+                    0,
+                    new Date(2008,  11, 21)));
+        }
+
+        BookSorter bookSorter = new OrderByTitle();
+        bookSorter.sortBy(books);
+        for (Book b: books) {
+            System.out.println(b);
+        }
+
+    }
+
 
 }
