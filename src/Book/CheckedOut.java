373d35cf3c5604aac368b8e5fed1377ead12b777
Ben Radley <bdr1224@rit.edu>
2020-10-21 20:08:32 -0400

    Added skeleton frameworks for the Concrete States InStock and CheckedOut

--- src/Book/CheckedOut.java
+++ src/Book/CheckedOut.java
@@ -1,2 +1,12 @@
 package Book;
 
+public class CheckedOut{
+    protected Book book;
+
+    public CheckedOut(Book book) {
+        this.book = book;
+    }
+    public boolean isBookAvailable(){
+        return false;
+    }
+}
--- src/Book/InStock.java
+++ src/Book/InStock.java
@@ -1,2 +1,12 @@
 package Book;
 
+public class InStock{
+    protected Book book;
+
+    public InStock(Book book) {
+        this.book = book;
+    }
+    public boolean isBookAvailable(){
+        return false;
+    }
+}
