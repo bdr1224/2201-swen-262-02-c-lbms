2c913e0e8fbe09a0411f677a78ae7ef051eea068
Ben Radley <bdr1224@rit.edu>
2020-11-17 19:01:16 -0500

    Added Command interface for Undo/Redo

---
+++ src/Visitor/Command.java
@@ -0,0 +1,14 @@
+package Visitor;
+
+/**
+ * Interface for subsystems with the Command pattern
+ *
+ * @author Ben Radley
+ */
+public interface Command {
+    /*
+    * Method to execute a command
+    */
+
+    String execute();
+}
