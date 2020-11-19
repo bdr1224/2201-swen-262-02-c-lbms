8964975f5cceb7eae6f54850d4aaa4ece8fca884
Ben Radley <bdr1224@rit.edu>
2020-11-19 13:15:44 -0500

    Added skeleton file for Return method for display and partial development of Borrow class

--- src/Books/Borrow.java
+++ src/Books/Borrow.java
@@ -6,4 +6,28 @@ import java.util.ArrayList;
 
 
 public class Borrow implements Command{
+    private Long visitorID;
+    private ArrayList<String> bookISBN;
+    private boolean canUndo;
+
+    public Borrow(Long visitorID, ArrayList<String> bookISBN, boolean canUndo){
+        this.visitorID=visitorID;
+        this.bookISBN=bookISBN;
+        this.canUndo=canUndo;
+    }
+
+    @Override
+    public String execute(){
+        try{
+            if(this.canUndo){
+                //Application a = new Application();
+            }
+            else{
+                return "";
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
